9d6dea97f21bda35bc99822e8947aabc76a1a99a
Tarfala <jon.risberg@gmail.com>
2019-02-25 07:56:06 +0100

    no message

--- NextPlanetUnity/NextPlan(et)/Assets/BoatPlayer.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/BoatPlayer.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class BoatPlayer : MonoBehaviour
 {
-   
 
+    public GameObject PlayerObject;
+    public GameObject BoatObject;
     void Start()
     {
+        PlayerObject.GetComponent<Renderer>().enabled = true;
+
+        BoatObject.GetComponent<Renderer>().enabled = false;
+
+
     }
 
     // Update is called once per frame
--- NextPlanetUnity/NextPlan(et)/Assets/Scripts/BoatScript.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/BoatScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BoatScript : MonoBehaviour
 {
+    public GameObject BoatObject;
+    public GameObject PlayerObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BoatObject.GetComponent<Renderer>().enabled = false;
+        PlayerObject.GetComponent<Renderer>().enabled = true;
     }
 
     // Update is called once per frame
