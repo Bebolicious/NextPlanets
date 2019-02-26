00806ae9ee365e81cb4d62fe230f0e73a46a2bc7
Tarfala <jon.risberg@gmail.com>
2019-02-26 09:07:21 +0100

    no message

---
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/FireExScripy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExScripy : Collidable
+{
+    public GameObject FireOne;
+    public GameObject FireTwo;
+    public GameObject Ex;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+        {
+            FireOne.SetActive(false);
+            FireTwo.SetActive(false);
+            Ex.SetActive(true);
+        }
+    }
+    
+}
