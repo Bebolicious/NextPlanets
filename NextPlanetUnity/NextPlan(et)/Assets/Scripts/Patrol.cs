25a90ec582f332f9db06bdb68ee6fa31467a2d04
Bebolicious <a.zetterberg@hotmail.com>
2019-02-27 13:06:01 +0100

    hejhej

---
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+
+    public Transform[] MoveSpots;
+    private int Ranspot;
+    public float startWaitTime;
+    private float waitTime;
+
+    private void Start()
+    {
+        Ranspot = Random.Range(0, MoveSpots.Length);
+
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, MoveSpots[Ranspot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, MoveSpots[Ranspot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                Ranspot = Random.Range(0, MoveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+
+    }
+}
