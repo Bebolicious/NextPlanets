e8198458b68d9f12bc3112a06a0c5bcb3bd5a6b5
Tarfala <jon.risberg@gmail.com>
2019-02-27 20:35:13 +0100

    QuestHelperPåPlaneten

--- NextPlanetUnity/NextPlan(et)/Assets/PlanetNPCHelper.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/PlanetNPCHelper.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeachQuest : Collidable
+public class PlanetNPCHelper : Collidable
 {
     public string[] message;  // The NPC's message, he will shout it on contact, change in the inspector
     private float cooldown = 4.0f;
     private float lastShout;
     private bool questDone;
     
+
     // Overrides
     protected override void Start()
     {
-
         base.Start();
         lastShout = -cooldown;
     }
@@ -23,10 +23,20 @@ public class BeachQuest : Collidable
             lastShout = Time.time;
             GameManager.instance.ShowText(message[0], 25, Color.white, transform.position + new Vector3(0, 0.32f, 0), Vector3.zero, cooldown);
         }
-            if (Time.time - lastShout > cooldown && GameManager.instance.beachQuest == true)
+        else if (Time.time - lastShout > cooldown && GameManager.instance.craterQuest == false)
         {
             lastShout = Time.time;
             GameManager.instance.ShowText(message[1], 25, Color.white, transform.position + new Vector3(0, 0.32f, 0), Vector3.zero, cooldown);
+        }
+        else if (Time.time - lastShout > cooldown && GameManager.instance.flowerQuest == false)
+        {
+            lastShout = Time.time;
+            GameManager.instance.ShowText(message[2], 25, Color.white, transform.position + new Vector3(0, 0.32f, 0), Vector3.zero, cooldown);
+        }
+        else if (Time.time - lastShout > cooldown)
+        {
+            lastShout = Time.time;
+            GameManager.instance.ShowText(message[3], 25, Color.white, transform.position + new Vector3(0, 0.32f, 0), Vector3.zero, cooldown);
         }
         
     }
