96a3cc6dfcd82312b704a9614f997b24080d3907
RIProG <riprog@hotmail.com>
2019-02-27 11:03:21 +0100

    Monster, boxcollision på mountainpassage, mountaindoor och player.

--- NextPlanetUnity/NextPlan(et)/Assets/Scripts/MountainDoor.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/MountainDoor.cs
@@ -16,16 +16,22 @@ public class MountainDoor : Collidable
     }
     protected override void OnCollide(Collider2D coll)
     {
+        if (coll.name == "Player")
+        {
         if (Time.time - lastShout > cooldown && GameManager.instance.flowerQuest == false)
         {
+            Debug.Log("Door1");
             lastShout = Time.time;
             GameManager.instance.ShowText(message[0], 25, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, cooldown);
         }
-        else if (Time.time - lastShout > cooldown && GameManager.instance.flowerQuest == false)
+        else if (Time.time - lastShout > cooldown && GameManager.instance.flowerQuest == true)
         {
+            Debug.Log("Door2");
             lastShout = Time.time;
             GameManager.instance.ShowText(message[1], 25, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, cooldown);
             UnityEngine.SceneManagement.SceneManager.LoadScene("Basement");
+        }
+
         }
         
     }
