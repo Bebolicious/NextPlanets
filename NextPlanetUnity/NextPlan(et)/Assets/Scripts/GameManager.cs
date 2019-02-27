fb1d88c178630b3bca88cf3bf26c12281627d53f
Bebolicious <a.zetterberg@hotmail.com>
2019-02-27 10:07:39 +0100

    no message

--- NextPlanetUnity/NextPlan(et)/Assets/Scripts/GameManager.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
         SceneManager.sceneLoaded += LoadState;
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
+    
 
     // Resources
     public List<Sprite> playerSprites;
