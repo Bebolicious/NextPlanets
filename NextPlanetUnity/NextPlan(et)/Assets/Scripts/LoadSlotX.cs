0ede25d0526ac230dbf5b0d21b2e24ba077f4f2b
Tarfala <jon.risberg@gmail.com>
2019-03-02 23:18:32 +0100

    Tog bort databsen, går ej att ladda spel

--- NextPlanetUnity/NextPlan(et)/Assets/Scripts/LoadSlotX.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/LoadSlotX.cs
@@ -10,11 +10,10 @@ public class LoadSlotX : MonoBehaviour
     
     bool isDataBaseChecked;
     string RawData = "";
-    private const string conString = "Server=tcp:academyunity.database.windows.net,1433;Initial Catalog=mysqlserver;Persist Security Info=False;User ID=AcademyAdmin;Password=pOWERkING!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
 
     public void getDataBaseInput(int id)
     {
-        if (id == 0 && isDataBaseChecked == false)
+        if (id != 5 && isDataBaseChecked == false)
         {
             GameManager.instance.credits = 0;
             GameManager.instance.fireFlowers = 0;
@@ -30,72 +29,8 @@ public class LoadSlotX : MonoBehaviour
             isDataBaseChecked = true;
             SceneManager.LoadScene("SpaceShip");
 
-
         }
-        if (isDataBaseChecked == false)
-        {
-            var sql = @"select * from SaveData where id = " + id.ToString();
-            using (SqlConnection connection = new SqlConnection(conString))
-            using (SqlCommand command2 = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                SqlDataReader reader = command2.ExecuteReader();
-                while (reader.Read())
-                {
-                    RawData = reader.GetSqlString(1).ToString();
-                }
-
-            }
-            string[] data = RawData.Split('|');
-            GameManager.instance.credits = int.Parse(data[0]);
-            GameManager.instance.fireFlowers = int.Parse(data[1]);
-            GameManager.instance.savedTulics = int.Parse(data[2]);
-            GameManager.instance.experience = int.Parse(data[3]);
-            GameManager.instance.ScenName = data[4];
-            if (int.Parse(data[5]) == 0)
-            {
-                GameManager.instance.flowerQuest = false;
-            }
-            else
-            {
-                GameManager.instance.flowerQuest = true;
-            }
-            if (int.Parse(data[6]) == 0)
-            {
-                GameManager.instance.caveQuest = false;
-            }
-            else
-            {
-                GameManager.instance.caveQuest = true;
-            }
-            if (int.Parse(data[7]) == 0)
-            {
-                GameManager.instance.hasWeapon = false;
-            }
-            else
-            {
-                GameManager.instance.hasWeapon = true;
-            }
-            if (int.Parse(data[8]) == 0)
-            {
-                GameManager.instance.beachQuest = false;
-            }
-            else
-            {
-                GameManager.instance.beachQuest = true;
-            }
-            if (int.Parse(data[9]) == 0)
-            {
-                GameManager.instance.craterQuest = false;
-            }
-            else
-            {
-                GameManager.instance.craterQuest = true;
-            }
-            GameManager.instance.SaveSlot = int.Parse(data[10]);
-            isDataBaseChecked = true;
-            SceneManager.LoadScene(data[4]);
-        }
+       
 
     }
 }
--- NextPlanetUnity/NextPlan(et)/Assets/Scripts/Portal.cs
+++ NextPlanetUnity/NextPlan(et)/Assets/Scripts/Portal.cs
@@ -7,69 +7,69 @@ public class Portal : Collidable
 {
     public string[] sceneNames;
 
-    private const string conString = "Server=tcp:academyunity.database.windows.net,1433;Initial Catalog=mysqlserver;Persist Security Info=False;User ID=AcademyAdmin;Password=pOWERkING!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+    private const string conString = "";// Servernamn + inlogg här
 
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.name == "Player")
         {
-            int saveSlot = GameManager.instance.SaveSlot;
-            string s = "";
-            string sceneName = sceneNames[0];            
-            s += GameManager.instance.credits.ToString() + "|";
-            s += GameManager.instance.fireFlowers.ToString() + "|";
-            s += GameManager.instance.savedTulics.ToString() + "|";
-            s += GameManager.instance.experience.ToString() + "|";
-            s += sceneName + "|";
-            if (GameManager.instance.flowerQuest == false)
-            {
-                s += 0.ToString() + "|";
-            }
-            if (GameManager.instance.flowerQuest == true)
-            {
-                s += 1.ToString() + "|";
-            }
-            if (GameManager.instance.caveQuest == false)
-            {
-                s += 0.ToString() + "|";
-            }
-            if (GameManager.instance.caveQuest == true)
-            {
-                s += 1.ToString() + "|";
-            }
-            if (GameManager.instance.hasWeapon == false)
-            {
-                s += 0.ToString() + "|";
-            }
-            if (GameManager.instance.hasWeapon == true)
-            {
-                s += 1.ToString() + "|";
-            }
-            if (GameManager.instance.beachQuest == false)
-            {
-                s += 0.ToString() + "|";
-            }
-            if (GameManager.instance.beachQuest == true)
-            {
-                s += 1.ToString() + "|";
-            }
-            if (GameManager.instance.craterQuest == false)
-            {
-                s += 0.ToString() + "|";
-            }
-            if (GameManager.instance.craterQuest == true)
-            {
-                s += 1.ToString() + "|";
-            }
-            s += GameManager.instance.SaveSlot.ToString();
-            var sql = "UPDATE SaveData SET RawData = @s where id = " + saveSlot;
-            using (SqlConnection connection = new SqlConnection(conString))
-            using (SqlCommand command = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                command.Parameters.Add(new SqlParameter("@s", s));
-                command.ExecuteNonQuery();
-            }
+            //int saveSlot = GameManager.instance.SaveSlot;
+            //string s = "";
+            string sceneName = sceneNames[0];
+            //s += GameManager.instance.credits.ToString() + "|";
+            //s += GameManager.instance.fireFlowers.ToString() + "|";
+            //s += GameManager.instance.savedTulics.ToString() + "|";
+            //s += GameManager.instance.experience.ToString() + "|";
+            //s += sceneName + "|";
+            //if (GameManager.instance.flowerQuest == false)
+            //{
+            //    s += 0.ToString() + "|";
+            //}
+            //if (GameManager.instance.flowerQuest == true)
+            //{
+            //    s += 1.ToString() + "|";
+            //}
+            //if (GameManager.instance.caveQuest == false)
+            //{
+            //    s += 0.ToString() + "|";
+            //}
+            //if (GameManager.instance.caveQuest == true)
+            //{
+            //    s += 1.ToString() + "|";
+            //}
+            //if (GameManager.instance.hasWeapon == false)
+            //{
+            //    s += 0.ToString() + "|";
+            //}
+            //if (GameManager.instance.hasWeapon == true)
+            //{
+            //    s += 1.ToString() + "|";
+            //}
+            //if (GameManager.instance.beachQuest == false)
+            //{
+            //    s += 0.ToString() + "|";
+            //}
+            //if (GameManager.instance.beachQuest == true)
+            //{
+            //    s += 1.ToString() + "|";
+            //}
+            //if (GameManager.instance.craterQuest == false)
+            //{
+            //    s += 0.ToString() + "|";
+            //}
+            //if (GameManager.instance.craterQuest == true)
+            //{
+            //    s += 1.ToString() + "|";
+            //}
+            //s += GameManager.instance.SaveSlot.ToString();
+            //var sql = "UPDATE SaveData SET RawData = @s where id = " + saveSlot;
+            //using (SqlConnection connection = new SqlConnection(conString))
+            //using (SqlCommand command = new SqlCommand(sql, connection))
+            //{
+            //    connection.Open();
+            //    command.Parameters.Add(new SqlParameter("@s", s));
+            //    command.ExecuteNonQuery();
+            //}
 
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
 
